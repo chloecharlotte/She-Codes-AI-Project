3c8ff7e94793b76569d6ed4078ebc850f8d2fca6
chloecharlotte <159797277+chloecharlotte@users.noreply.github.com>
2025-05-31 19:04:49 -0500

    Add AI element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateBook(event) {
-  event.preventDefault();
-
+function displayBook(response) {
   new Typewriter("#book-recommendation", {
-    strings: ["Hello", "World"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: null,
   });
+}
+
+function generateBook(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "14e2ba01dt6565f0b8o8e18b0aa7193f";
+  let context =
+    "you are an intellegent AI assistant that was created to help recommend books to people based on the user instructions provided. Please only recommend one book and do no give reasoning to the recommendations.";
+  let prompt = `user instructions: generate a book about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayBook);
 }
 
 let bookFormGenerator = document.querySelector("#book-generator");
